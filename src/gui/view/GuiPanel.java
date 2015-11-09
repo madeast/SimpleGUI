7e6ba8970e529b1501bba9dea8c8314f3d154aaf
Easton Madsen <eeagle25@comcast.net>
2015-11-09 09:47:47 -0700

    Added Spring Layout

--- src/gui/view/GuiPanel.java
+++ src/gui/view/GuiPanel.java
@@ -4,18 +4,21 @@ import javax.swing.JPanel;
 import gui.controller.GuiController;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout; //For Layout
 
 public class GuiPanel extends JPanel
 {
 	private GuiController baseController;
 	private JButton firstButton;
 	private JTextField firstTextField;
+	private SpringLayout baseLayout;
 	
 	public GuiPanel(GuiController baseController)
 	{
 		this.baseController = baseController;
 		
-		firstButton = new JButton("Please do not push me!!");
+		baseLayout = new SpringLayout();
+		firstButton = new JButton("Don't push me, cause I'm close to the edge!");
 		firstTextField = new JTextField("Words should be typed here.");
 		
 		setupPanel();
@@ -25,7 +28,8 @@ public class GuiPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		
+		this.add(firstButton);
+		this.add(firstTextField);
 	}
 	
 	private void setupLayout()
