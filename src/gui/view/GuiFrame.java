5500847ee99cf98bae15465b32811b5bc33f31fd
Easton Madsen <eeagle25@comcast.net>
2015-11-09 09:39:35 -0700

    Added the button to the frame.

--- src/gui/view/GuiFrame.java
+++ src/gui/view/GuiFrame.java
@@ -6,19 +6,28 @@ import gui.controller.GuiController;
 public class GuiFrame extends JFrame
 {
 	private GuiController baseController;
+	private GuiPanel basePanel;
 	
 	public GuiFrame(GuiController baseController)
 	{
 		this.baseController = baseController;
+		basePanel = new GuiPanel(baseController);
 		setupFrame();
 		
 	}
 	
 	private void setupFrame()
 	{
-		this.setSize(400,400);
-		this.setResizable(false);
-		this.setVisible(true);
+		this.setContentPane(basePanel); // Must be first line of setupFrame - Installs the panel in the frame.
+		this.setSize(400,400); //Find a good size for the app.
+		this.setTitle("Simple Gui!!");
+		this.setResizable(false); //advisable not required.
+		this.setVisible(true); // Must be last line of setupFrame!
+	}
+	
+	public GuiController getBaseController()
+	{
+		return baseController;
 	}
 
 }
--- src/gui/view/GuiPanel.java
+++ src/gui/view/GuiPanel.java
@@ -2,14 +2,40 @@ package gui.view;
 
 import javax.swing.JPanel;
 import gui.controller.GuiController;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class GuiPanel extends JPanel
 {
 	private GuiController baseController;
+	private JButton firstButton;
+	private JTextField firstTextField;
 	
 	public GuiPanel(GuiController baseController)
 	{
 		this.baseController = baseController;
 		
+		firstButton = new JButton("Please do not push me!!");
+		firstTextField = new JTextField("Words should be typed here.");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
 	}
 }
+
