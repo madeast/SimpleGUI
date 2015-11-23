f07eae4ba9f4e02e2f48313f8ca70ed288c49633
Easton Madsen <eeagle25@comcast.net>
2015-11-23 08:54:45 -0700

    Implemented MouseListener

--- src/gui/view/GuiPanel.java
+++ src/gui/view/GuiPanel.java
@@ -19,7 +19,7 @@ public class GuiPanel extends JPanel
 		baseLayout = new SpringLayout();
 		firstButton = new JButton("Don't push me, cause I'm close to the edge!");
 		firstTextField = new JTextField("Words should be typed here.");
-
+		
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -34,7 +34,10 @@ public class GuiPanel extends JPanel
 
 	private void setupLayout()
 	{
-
+		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 10, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstButton, 53, SpringLayout.SOUTH, firstTextField);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 36, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 0, SpringLayout.WEST, firstButton);
 	}
 	
 	private void changeRandomColor()
@@ -53,27 +56,48 @@ public class GuiPanel extends JPanel
 		{
 			public void mouseClicked(MouseEvent click)
 			{
-				
+				//changeRandomColor();
 			}
 			
 			public void mouseReleased(MouseEvent released)
 			{
-				
+				//changeRandomColor();
 			}
 			
 			public void mousePressed(MouseEvent pressed)
 			{
-				
+				//changeRandomColor();
 			}
 			
 			public void mouseEntered(MouseEvent entered)
 			{
-				
+				//changeRandomColor();
 			}
 			
 			public void mouseExited(MouseEvent exited)
 			{
-				
+				//changeRandomColor();
+			}
+		});
+		
+		this.addMouseMotionListener(new MouseMotionListener()
+		{
+			public void mouseMoved(MouseEvent moved)
+			{
+				if(moved.isAltDown())
+				{
+					changeRandomColor();
+				}
+				firstTextField.setText("Mouse X: " + moved.getX() + " Mouse Y: " + moved.getY() );
+				if((Math.abs(moved.getX() - firstButton.getX()) < 5 && (Math.abs(moved.getY() - firstButton.getY()) < 5)))
+				{
+					firstButton.setLocation((int) (Math.random() * 400 ), (int) (Math.random() * 400));
+				}
+			}
+			
+			public void  mouseDragged(MouseEvent dragged)
+			{
+				//changeRandomColor();
 			}
 		});
 		
